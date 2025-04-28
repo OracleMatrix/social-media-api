9ffed44e135d3757536c45e54c287ea911f045bf
oraclematrix <ehsanmohamadipoor@gmail.com>
2025-04-28 19:37:53 +0330

    remove unused database import from comments route

--- src/routes/comments.route.js
+++ src/routes/comments.route.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../models");
 const auth = require("../middlewares/auth");
 const commentsController = require("../controllers/comments.controller");
 
