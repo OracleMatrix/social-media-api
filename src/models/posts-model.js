da4f4f8dff16318586b38ea4eda6841abc31fac2
ehsanmp <ehsanmohamadipoor@gmail.com>
2025-06-02 11:17:29 +0330

    added postPicture column to model

--- src/models/posts-model.js
+++ src/models/posts-model.js
@@ -31,6 +31,10 @@ module.exports = (sequelize) => {
         type: DataTypes.STRING,
         allowNull: false,
       },
+      postPicture: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
       userId: {
         type: DataTypes.INTEGER,
         references: {
